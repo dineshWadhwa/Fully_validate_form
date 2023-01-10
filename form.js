94bfd80a9d46e98a8b5a18e8d43149dd98b491d0
dineshWadhwa <sherry12dinesh@gmail.com>
2023-01-10 13:01:49 +0530

    alldone

--- form.js
+++ form.js
@@ -2,26 +2,28 @@ function fnamevalid() {
   // first name
   let regex = /^[a-zA-Z]{2,}$/;
   let name = document.getElementById("name").value;
+  console.log(name)
   if (regex.test(name)) {
-    console.log("match");
+    // console.log("match"); 
     let name = document.getElementById("name");
     name.classList.remove("is-invalid");
   } else {
-    console.log("not matched");
+    // console.log("not matched");
     let name = document.getElementById("name");
     name.classList.add("is-invalid");
   }
 }
 
+
 function lnamevalid() {
   let regex = /^[a-zA-Z]{2,}$/;
   let lname = document.getElementById("lname").value;
   if (regex.test(lname)) {
-    console.log("match");
+    // console.log("match");
     let lname = document.getElementById("lname");
     lname.classList.remove("is-invalid");
   } else {
-    console.log("not matched");
+    // console.log("not matched");
     let lname = document.getElementById("lname");
     lname.classList.add("is-invalid");
   }
@@ -32,11 +34,11 @@ function pvalid() {
   let regexp = /^([+]\d{2}[ ])?\d{10}$/;
   let phone = document.getElementById("phone").value;
   if (regexp.test(phone)) {
-    console.log("match");
+    // console.log("match");
     let phone = document.getElementById("phone");
     phone.classList.remove("is-invalid");
   } else {
-    console.log("not matched");
+    // console.log("not matched");
     let phone = document.getElementById("phone");
     phone.classList.add("is-invalid");
   }
@@ -46,11 +48,11 @@ function evalid() {
   let regexe = /^\S+@\S+\.\S+$/;
   let email = document.getElementById("email").value;
   if (regexe.test(email)) {
-    console.log("match");
+    // console.log("match");
     let email = document.getElementById("email");
     email.classList.remove("is-invalid");
   } else {
-    console.log("not matched");
+    // console.log("not matched");
     let email = document.getElementById("email");
     email.classList.add("is-invalid");
   }
@@ -64,7 +66,7 @@ function btnd() {
   let email = document.getElementById("email").value;
   let phone = document.getElementById("phone").value;
   let dob = document.getElementById("dob").value;
-  let regex = /^[a-zA-Z]${2,}/;
+  let regex = /^[a-zA-Z]{2,}$/;
   let regexp = /^([+]\d{2}[ ])?\d{10}$/;
   let regexe = /^\S+@\S+\.\S+$/;
 
@@ -80,11 +82,11 @@ function btnd() {
     document.getElementById("boxcheck")
     let submit = document.getElementById("submit");
   submit.disabled = false;
-  console.log("correct")
+  // console.log("correct")
   
   } else {
     document.getElementById("boxcheck").checked = false;
     alert("Fill Correct information")
-    console.log("test")
+    // console.log("test")
   }
 }
